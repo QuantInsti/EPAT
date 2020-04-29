51a892cfe055496be1269faab98aee5ae5287fb9
ishanshah15 <40386408+ishanshah15@users.noreply.github.com>
2020-04-29 11:51:53 +0530

    Update optimization.py

--- Statistical Arbitrage/optimization.py
+++ Statistical Arbitrage/optimization.py
@@ -4,25 +4,29 @@ import matplotlib.pyplot as plt
 
 # Mean reversion strategy
 def mrs_pnl(lookback,std_dev,df):
+    # Compute Bollinger Bands
     df['moving_average'] = df.prices.rolling(lookback).mean()
     df['moving_std_dev'] = df.prices.rolling(lookback).std()
     df['upper_band'] = df.moving_average + std_dev*df.moving_std_dev
     df['lower_band'] = df.moving_average - std_dev*df.moving_std_dev
+    
+    # Check for long and short positions
     df['long_entry'] = df.prices < df.lower_band   
     df['long_exit'] = df.prices >= df.moving_average
     df['short_entry'] = df.prices > df.upper_band   
     df['short_exit'] = df.prices <= df.moving_average
     df['positions_long'] = np.nan  
-    df.loc[df.long_entry,'positions_long']= 1  
-    df.loc[df.long_exit,'positions_long']= 0  
+    df.loc[df.long_entry,'positions_long'] = 1  
+    df.loc[df.long_exit,'positions_long'] = 0  
     df['positions_short'] = np.nan  
-    df.loc[df.short_entry,'positions_short']= -1  
-    df.loc[df.short_exit,'positions_short']= 0  
+    df.loc[df.short_entry,'positions_short'] = -1  
+    df.loc[df.short_exit,'positions_short'] = 0  
     df = df.fillna(method='ffill')  
     df['positions'] = df.positions_long + df.positions_short
-    df['prices_difference']= df.prices - df.prices.shift(1)
-    df['daily_returns'] = df.prices_difference /df.prices.shift(1)
-    df['pnl'] = df.positions.shift(1) * df.daily_returns
+    
+    # Calculate the PnL
+    df['prices_difference'] = df.prices - df.prices.shift(1)
+    df['pnl'] = df.positions.shift(1) * df.prices_difference
     df['cumpnl'] = df.pnl.cumsum()
     return df.cumpnl.iloc[-1]
 
